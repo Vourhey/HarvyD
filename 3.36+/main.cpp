e2640dd4a223ec7f5c80acee8d969a4e656f0754
Vourhey <vadim.razorq@gmail.com>
2011-06-14 23:07:32 +0400

    Реорганизация проекта и перефраз цели

--- 3.36+/main.cpp
+++ 3.36+/main.cpp
@@ -9,7 +9,6 @@
 using namespace std;
 
 typedef unsigned int uint;
-typedef map<char*, uint> my_map;
 
 inline void help()
 {
@@ -19,83 +18,6 @@ inline void help()
 	    "Любой из них можно указать после имени программы.\n"
 	    "Для выхода введите -1.\n";
 }
-
-/*
-void loadtable(map<char*, uint> *m, fstream *s)
-{
-    cerr << "Load Table\n";
-    char name[10] = {'\0'};
-    uint score = 0;
-
-    for(int i=0; i<10; ++i)
-    {
-	s->read(name, 10);
-	s->read((char*)&score, 4);
-	cerr << name << " - " << score << '\n';
-	m->insert(pair<char*, uint>(name, score));
-    }
-}
-
-class map_comp
-{
-public:
-    bool operator()(pair<char*, uint> p1, pair<char*, uint> p2)
-    { return p1.second < p2.second; }
-} map_obj;
-
-void my_swap(pair<char*const,uint> &p1, pair<char*const,uint> &p2)
-{
-    swap(p1.second, p2.second);
-
-    char n[10];
-    memcpy(n, p1.first, 10);
-    memcpy(p1.first, p2.first, 10);
-    memcpy(p2.first, n, 10);
-}
-
-void my_sort(my_map::iterator begin, my_map::iterator end, map_comp &comp)
-{
-    while(begin != end)
-    {
-	my_map::iterator t = begin; 
-	++t;
-	while(t != end)
-	{
-	    if(comp(*begin, *t))
-		my_swap(*begin, *t);
-	    ++t;
-	}
-	++begin;
-    }
-}
-
-void savetable(my_map *m, fstream *s)
-{
-    cerr << "Save Table\n";
-    s->seekg(0, ios::beg);
-
-    my_sort(m->begin(), m->end(), map_obj);
-
-    int i=0;    
-    for(my_map::iterator it = m->begin(); it != m->end(); ++it)
-    {
-	s->write(it->first, 10);
-	s->write((char*)&it->second, 4);
-	if(i == 9) // записываем только 10 значений
-	    break; 
-	++i;
-    }
-}
-
-void insert_in_table(my_map *m, int score)
-{
-    char name[10] = {'\0'};
-    cout << "Введите ваше имя: ";
-    cin.getline(name, 10);
-    
-    m->insert(pair<char*, uint>(name, score));
-}
-*/
 
 int main(int argc, char **argv)
 {
@@ -113,13 +35,6 @@ int main(int argc, char **argv)
 	    		  "Неверно. Попытайтесь ещё раз. ",
 			  "Не опускайте руки! ",
 			  "Нет. Продолжайте ваши попытки. " };
-/*    fstream table("table", fstream::in | fstream::out | fstream::binary);
-    my_map tmap;
-
-    if(!table.is_open())
-	cout << "Warning: файл результатов не открыт\n";
-
-    loadtable(&tmap, &table); */
 
     srand(time(NULL)); // инициализируем генератор
 
@@ -173,7 +88,6 @@ otvet:
 	    {
 		cout << "Вы проиграли. Ваш счет " 
 		     << yes_counter << '\n';
-//		insert_in_table(&tmap, yes_counter);
 		break;
 	    }
 
@@ -181,9 +95,6 @@ otvet:
 	    goto otvet;   // никогда не пользовался goto :)
 	}
     }
-/*
-    savetable(&tmap, &table);
-    table.close(); */
 
     return 0;
 }
